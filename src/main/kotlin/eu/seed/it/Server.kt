7ba787b1fac2ca28e554ee7e6ab7b73e24373303
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2019-10-23 23:49:06 +0200

    Add gzip encoding + json content type for every request by default

--- src/main/kotlin/eu/seed/it/Server.kt
+++ src/main/kotlin/eu/seed/it/Server.kt
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import spark.Filter
 import spark.Spark.*
 
 
@@ -23,19 +24,23 @@ class Server(private val connection: Connection, private val database: Database)
         logger.info("Listening on $connection")
         port(connection.port)
 
-        get("/seeds") { _, res ->
-            res.type("application/json")
-
+        get("/seeds") { _, _ ->
             val seeds = database.seeds()
             mapper.writeValueAsString(seeds)
         }
 
-        notFound { _, res ->
-            res.type("application/json")
+        notFound { _, _ ->
             val objectNode = mapper.createObjectNode()
             objectNode.put("msg", "404")
             objectNode.toString()
         }
+
+        val gzipFilter = Filter { _, res ->
+            res.header("Content-Encoding", "gzip")
+            res.type("application/json")
+        }
+
+        after(gzipFilter)
 
     }
 }
