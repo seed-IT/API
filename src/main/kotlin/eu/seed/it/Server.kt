5a974ee5a6f533b1a24f3f4049de7a72233b353c
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2019-10-23 19:26:40 +0200

    Add various stuff..

--- src/main/kotlin/eu/seed/it/Server.kt
+++ src/main/kotlin/eu/seed/it/Server.kt
@@ -1,8 +1,15 @@
 package eu.seed.it
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import spark.Spark.*
+
 class Server(private val connection: Connection) {
+    private val logger: Logger = LoggerFactory.getLogger("Server")
+
     fun serve() {
-        println("Listening on $connection")
-        println("Not implemented yet")
+        logger.info("Listening on $connection")
+        port(connection.port)
+        get("/hello") { req, res -> "Hello World" }
     }
 }
