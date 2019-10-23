a2f1212aa053ef9f5050a1633c4e8fc76a607b32
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2019-10-23 18:39:31 +0200

    Add config loading

--- src/main/kotlin/eu/seed/it/Main.kt
+++ src/main/kotlin/eu/seed/it/Main.kt
@@ -1,5 +1,36 @@
 package eu.seed.it
 
+import com.electronwill.nightconfig.core.file.FileConfig
+import java.io.File
+
+
 fun main(args: Array<String>) {
     println("Hello..")
+
+    loadConfig()
 }
+
+fun loadConfig() {
+    // TODO: find a better path
+    val cwd = System.getProperty("user.dir")
+    val configFile = File(cwd, "config.toml")
+
+    if (!configFile.exists()) System.err.println("${configFile.path} does not exist")
+    else println("Loading config from ${configFile.path}")
+
+    val conf = FileConfig.of(configFile)
+    conf.load()
+
+    val serverAddress = conf.getOrElse("server.address", "127.0.0.1")
+    val serverPort = conf.getIntOrElse("server.port", 4000)
+    val serverConnection = Connection(serverAddress, serverPort)
+
+    val databaseAddress = conf.getOrElse("database.address", "127.0.0.1")
+    val databasePort = conf.getIntOrElse("database.port", 3306)
+    val databaseConnection = Connection(databaseAddress, databasePort)
+
+    println(serverConnection)
+    println(databaseConnection)
+}
+
+data class Connection(val address: String, val port: Int)
