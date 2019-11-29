c0149734deac2555c93bf8f8228796ac7f9aaba9
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2019-11-29 19:28:18 +0100

    Update tests to use dependency injection

--- src/test/kotlin/eu/seed/it/SerializationTest.kt
+++ src/test/kotlin/eu/seed/it/SerializationTest.kt
@@ -1,33 +1,34 @@
 package eu.seed.it
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect
-import com.fasterxml.jackson.annotation.PropertyAccessor
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.module.SimpleModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import eu.seed.it.database.Sensor
-import eu.seed.it.serialization.SensorDeserialiser
-import eu.seed.it.serialization.SensorSerializer
+import eu.seed.it.modules.jacksonModule
 import org.junit.jupiter.api.Test
+import org.kodein.di.Kodein
+import org.kodein.di.generic.instance
 import java.time.LocalDateTime
 import kotlin.test.assertEquals
 
 class SerializationTest {
-    val mapper: ObjectMapper = jacksonObjectMapper().findAndRegisterModules().apply {
-        setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
-        val module = SimpleModule()
-        module.addSerializer(Sensor::class.java, SensorSerializer())
-        module.addDeserializer(Sensor::class.java, SensorDeserialiser())
-        registerModule(module)
+    private val kodein = Kodein {
+        import(jacksonModule)
     }
 
+    private val mapper: ObjectMapper by kodein.instance()
 
     @Test
     fun `Sensor serialization Test`() {
         val dt = LocalDateTime.of(2019, 1, 1, 1, 1)
         val sensor = Sensor(dt, 22.1f, 22.1f, 22.1f)
-        val expected = """{"datetime":"2019-01-01T01:01:00","temperature":22.1,"humidity":22.1,"pressure":22.1}"""
+        val expected = """
+            {
+              "datetime" : "2019-01-01T01:01:00",
+              "temperature" : 22.1,
+              "humidity" : 22.1,
+              "pressure" : 22.1
+            }
+        """.trimIndent()
         val json = mapper.writeValueAsString(sensor)
         assertEquals(expected, json)
     }
